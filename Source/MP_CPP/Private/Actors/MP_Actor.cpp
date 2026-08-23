bb0839a593013d21d28dc98f0b454d88f56139e7
WangXu <W2446787821xu@outlook.com>
2026-08-23 16:46:02 +0800

    acter的replicate相关 1.Net Load On Client 2.Replicate：构造中设置用bool类型的 3.ReplicateMovement：构造函数中不能用虚函数进行设置

---
+++ Source/MP_CPP/Private/Actors/MP_Actor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/MP_Actor.h"
+
+// Sets default values
+AMP_Actor::AMP_Actor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	bNetLoadOnClient = true;
+	bReplicates = true;
+
+	/*
+		C++构造函数内部不要调用虚函数(核心原理)
+		构造阶段,子类对象还没构造完成,虚表还没切换到子类版本,虚函数只会执行当前类的实现,不会多态。
+		SetReplicateMovement(true);
+	*/
+	SetReplicatingMovement(true);
+	
+}
+
+// Called when the game starts or when spawned
+void AMP_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMP_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
